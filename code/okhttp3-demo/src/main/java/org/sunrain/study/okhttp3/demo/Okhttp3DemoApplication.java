f386c726ac62ff1984ac984a6795e2b3e559369e
齐超 <sunrain5921@qq.com>
2019-12-10 15:23:27 +0800

    update java code

---
+++ code/okhttp3-demo/src/main/java/org/sunrain/study/okhttp3/demo/Okhttp3DemoApplication.java
@@ -0,0 +1,13 @@
+package org.sunrain.study.okhttp3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Okhttp3DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Okhttp3DemoApplication.class, args);
+	}
+
+}
