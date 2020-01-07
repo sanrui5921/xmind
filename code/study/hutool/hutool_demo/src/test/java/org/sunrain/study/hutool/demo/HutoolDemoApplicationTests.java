66117705ba01ef6d705b849bd507f28135194274
齐超 <sunrain5921@qq.com>
2020-01-07 11:35:57 +0800

    add docker

---
+++ code/study/hutool/hutool_demo/src/test/java/org/sunrain/study/hutool/demo/HutoolDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sunrain.study.hutool.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HutoolDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
